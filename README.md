9aac6ac9e74caaf343e24eafcccd218ed4c06404
Anna Pawl <annampawl@gmail.com>
2020-02-05 13:28:37 -0800

    working on 'showChildren' toggle

---
+++ README.md
@@ -0,0 +1,10 @@
+## Description of Project 
+
+Use `array.map()`, `array.filter()`,`array.reduce()` to organize data in different ways. 
+
+## Background Information 
+The Titanic dataset can be downloaded in JSON from: [Titanic Dataset](https://public.opendatasoft.com/explore/dataset/titanic-passengers/export/)
+Data includes information like name, age, fare, passenger class, gender, etc. 
+
+## Getting Started/ How to Run Project 
+
--- titanic.js
+++ titanic.js
@@ -8,6 +8,7 @@ fetch('passengers.json')
 let state = false
 let showGender = false
 let showCasualties = false
+let showChildren = false
 const elements = []
 const passengerData = []
 
@@ -17,7 +18,6 @@ function handleData(json) {
 
   renderPassengers(fields, 'render-all-passengers')
   renderPassengers(fields, 'render-male-passengers')
-
 
   // Render Passenger Squares -------------------------
   renderPassengers(fields, 'render-all-passengers')
@@ -29,18 +29,14 @@ function handleData(json) {
     root.style.flexWrap = "wrap"
 
     data.forEach((passenger, i) => {
-      // Make element and attach to DOM
-      const el = document.createElement('div')
+      const el = document.createElement('div') // Make element and attach to DOM
       root.appendChild(el)
       elements.push(el)  //store element
       passengerData.push(passenger) //store passenger
 
-      el.style.width = "22px"
-      el.style.height = "22px"
-      el.style.backgroundColor = '#2b2b2b'
-      el.style.margin = "1px"
-      el.style.transition = '1s'
+      el.classList.add('square-styles')
       el.dataset.index = i // <div data-index="i">
+
     });
   }
 }
@@ -54,29 +50,25 @@ body.addEventListener('click', (e) => { //always takes event object as parameter
   if (index !== undefined)
     showOverlay(index)
   console.log(passengerData[index])
-
-
 })
 
 function showOverlay(index) {
   const el = document.getElementById('showOverlay')
-  const { name, sex, age, fare } = passengerData[index]
+  const { name, sex, age, survived, fare, pclass } = passengerData[index]
   el.style.display = 'block'
-  el.innerHTML =
-
-    el.innerHTML = `
-    ${name}
-    ${sex}
-    ${age}
-   ${fare}`
-  // el.style.height = '100px'
-  // el.style.width = '100px'
+  el.innerHTML = `
+  <div><span><strong>Name: </strong></span>${name}</div>
+  <div><span><strong>Gender: </strong></span>${sex}</div>
+  <div><span><strong>Age: </strong></span>${age}</div>
+  <div><span><strong>Survived: </strong></span>${survived}</div>
+  <div><span><strong>Passenger Class: </strong></span>${pclass}</div>
+  <div><span><strong>Fare: </strong></span>${fare}</div>
+   `
+  el.style.height = '140px'
+  el.style.width = '220px'
+  el.style.margin = '3px'
   el.style.border = '2px solid black'
 }
-
-
-
-// Remove from DOM
 
 // Toggle Gender Button On/Off -----------------------------
 const showGenderButton = document.getElementById('showGenderButton')
@@ -87,12 +79,15 @@ showGenderButton.addEventListener('click', (e) => {
     e.target.style.backgroundColor = '#3355a3'
     e.target.style.color = 'white'
     e.target.classList.add('buttonActive')
+    e.target.innerHTML = 'Reset'
     // show gender in el
     displayGender()
   } else {
     e.target.style.backgroundColor = 'white';
     e.target.style.color = 'black'
     e.target.classList.remove('buttonActive')
+    e.target.innerHTML = 'Gender'
+
     displayGender()
   }
 })
@@ -121,11 +116,13 @@ showCasualtiesButton.addEventListener('click', (e) => {
     e.target.style.backgroundColor = '#fdffba'
     e.target.classList.add('buttonActive')
     displayCasualties()
+    e.target.innerHTML = 'Reset'
 
   } else {
     e.target.style.backgroundColor = 'white';
     e.target.classList.remove('buttonActive')
     displayCasualties()
+    e.target.innerHTML = 'Casualties'
   }
 })
 
@@ -141,107 +138,37 @@ function displayCasualties() {
   })
 }
 
+// Toggle Childen Button On/Off -----------------------------
+const showChildrenButton = document.getElementById('showChildrenButton')
+showChildrenButton.addEventListener('click', (e) => {
+  showChildren = !showChildren
+  if (showChildren) {
+    e.target.classList.add('buttonActive')
+    e.target.innerHTML = 'Reset'
+    displayChildren()
+  } else {
+    e.target.classList.remove('buttonActive')
+    e.target.innerHTML = 'Gender'
+    displayChildren()
+  }
+})
 
-// // Total Passengers ----------------------------
-// const totalPassengers = fields.length
-// document.getElementById("total-passengers").innerHTML = totalPassengers
-// console.log("Total Passengers:" + totalPassengers)
+//  Display Children -------------------------------
+function displayChildren() {
 
-// // Survivors -----------------------------------
-// const Survivors = fields.filter(passenger => {
-//   return passenger.survived === "Yes"
-// })
-// document.getElementById("survivors").innerHTML = Survivors.length
-// console.log("Number of Survivors:" + Survivors.length)
+  const childPassengers = fields.filter(passenger => {
+    return passenger.age < 13
+  })
 
-// // Deaths ---------------------------------------
-// const Casualties = fields.filter(passenger => {
-//   return passenger.survived === "No"
-// })
-// document.getElementById('deaths').innerHTML = Casualties.length
-// console.log("Number of Casualties:" + Casualties.length)
-
-// // Child Passenger (Under 13) --------------------
-// const childPassengers = fields.filter(passenger => {
-//   return passenger.age < 13
-// })
-// document.getElementById('childPassengers').innerHTML = childPassengers.length
-// console.log("Total Number of Child Passengers: " + childPassengers.length)
-
-// // Number of Female Passengers  -----------------
-// const femalePassengers = fields.filter(passenger => {
-//   return passenger.sex === "female"
-// })
-// document.getElementById('femalePassengers').innerHTML = femalePassengers.length
-// console.log("Female Deaths: " + femaleDeaths.length)
-
-// // Number of Male Passengers --------------------
-// const malePassengers = fields.filter(passenger => {
-//   return passenger.sex === "male"
-// })
-// document.getElementById('malePassengers').innerHTML = malePassengers.length
-// console.log("Male Deaths: " + maleDeaths.length)
-
-// // Male Deaths ----------------------------------
-// const deadMen = malePassengers.reduce((acc, pass) => {
-//   if (pass.survived === "No") {
-//     acc += 1
-//   }
-//   return acc
-// }, 0)
-// document.getElementById('maleDeaths').innerHTML = deadMen
-// console.log("Male Deaths: " + deadMen)
-
-// // Female Deaths -------------------------------
-// const deadWomen = femalePassengers.reduce((acc, pass) => {
-//   if (pass.survived === "No") {
-//     acc += 1
-//   }
-//   return acc
-// }, 0)
-// document.getElementById('femaleDeaths').innerHTML = deadWomen
-// console.log("Female Deaths: " + deadWomen)
-
-// // Child Deaths (Under 13) ----------------------
-// const deadChildren = childPassengers.reduce((acc, child) => {
-//   if (child.survived === "No") {
-//     acc += 1
-//   }
-//   return acc
-// }, 0)
-// document.getElementById('childDeaths').innerHTML = deadChildren
-// console.log("Child Deaths: " + deadChildren)
-
-// // Challenge 5: Passenger Classes
-// // const pClasses = new Set()
-// // const numOfPClasses
-// // const pc = {}
-// // fields.forEach((passenger) => {
-// //   pClasses.add(passenger.pclass)
-// //   pc[passenger.pclass]
-// // })
-
-// // Challenge 1: Name, Fare, PClass, Survived Age
-// const allFares = fields
-//   .filter(passenger => passenger.fare !== undefined)
-//   .map(({ fare }) => fare)
-// console.log("All Fares:" + allFares)
-
-// // Stretch Challenge: Ages (All, Min, Max, and Range)
-// const allAges = fields.filter(passenger => {
-//   return passenger.age !== undefined
-// }).map(passenger => {
-//   return passenger.age
-// })
-// const minAge = Math.min(...allAges)
-// const maxAge = Math.max(...allAges)
-// const ageRange = maxAge - minAge
-
-// console.log('All Ages:' + allAges)
-// console.log('Min Age:' + minAge)
-// console.log('Max Age:' + maxAge)
-// console.log('Age Range:' + ageRange)
-
+  console.log(showChildren)
+  passengerData.forEach((obj, i) => {
+    const el = elements[i]
+    el.innerHTML = obj.childPassengers === "True" ? '☠︎' : ''
+    if (!showChilden) {
+      el.innerHTML = ''
+    }
+  })
+}
 
   //  Sort Button Function ------------------------
   // function sortButtons(button) {
